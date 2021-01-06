9a5649251b2aadd6cf167bab94aea328b2a71819
github-action-benchmark <github@users.noreply.github.com>
2021-01-06 01:38:45 +0000

    add OpenTelemetry Python Benchmarks - Python 3.5 - core (pytest) benchmark result for 87d7ced747177b6d840e069f935734a163216108

--- benchmarks/data.js
+++ benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1609897112995,
+  "lastUpdate": 1609897125005,
   "repoUrl": "https://github.com/lonewolf3739/opentelemetry-python",
   "entries": {
     "OpenTelemetry Python Benchmarks - Python 3.7 - core": [
@@ -920,6 +920,44 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.000005983264078034381",
             "extra": "mean: 68.49579594763188 usec\nrounds: 5577"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "srikanth.chekuri92@gmail.com",
+            "name": "Srikanth Chekuri",
+            "username": "lonewolf3739"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": false,
+          "id": "87d7ced747177b6d840e069f935734a163216108",
+          "message": "Add support for OTLP v0.6.0 (#1472)",
+          "timestamp": "2021-01-05T15:41:48-08:00",
+          "tree_id": "f5fca2206fc7bd721679294cd397ee47b729dcf2",
+          "url": "https://github.com/lonewolf3739/opentelemetry-python/commit/87d7ced747177b6d840e069f935734a163216108"
+        },
+        "date": 1609897123926,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "opentelemetry-sdk/tests/performance/benchmarks/trace/test_benchmark_trace.py::test_simple_start_span",
+            "value": 21000.229438332775,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0000048194834785149865",
+            "extra": "mean: 47.61852735640353 usec\nrounds: 1517"
+          },
+          {
+            "name": "opentelemetry-sdk/tests/performance/benchmarks/trace/test_benchmark_trace.py::test_simple_start_as_current_span",
+            "value": 13526.188046122285,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000013340037669466322",
+            "extra": "mean: 73.9306592951502 usec\nrounds: 5336"
           }
         ]
       }
