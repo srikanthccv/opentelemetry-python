86383704938039e9462e3dd3dae196bd0a47068d
github-action-benchmark <github@users.noreply.github.com>
2021-01-07 19:41:09 +0000

    add OpenTelemetry Python Benchmarks - Python 3.9 - exporter (pytest) benchmark result for cd39fc17b21baf68eddc4ddd0acfe2e80df59eae

--- benchmarks/data.js
+++ benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1610048464390,
+  "lastUpdate": 1610048469711,
   "repoUrl": "https://github.com/lonewolf3739/opentelemetry-python",
   "entries": {
     "OpenTelemetry Python Benchmarks - Python 3.7 - core": [
@@ -1844,6 +1844,44 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.0012487780131649148",
             "extra": "mean: 261.4695103661929 usec\nrounds: 5788"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "srikanth.chekuri92@gmail.com",
+            "name": "Srikanth Chekuri",
+            "username": "lonewolf3739"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": false,
+          "id": "cd39fc17b21baf68eddc4ddd0acfe2e80df59eae",
+          "message": "Update Jaeger exporter status code (#1488)",
+          "timestamp": "2021-01-07T08:05:29-08:00",
+          "tree_id": "2d2eb6aea5add7398b0ba85fe8a38da6ff809abb",
+          "url": "https://github.com/lonewolf3739/opentelemetry-python/commit/cd39fc17b21baf68eddc4ddd0acfe2e80df59eae"
+        },
+        "date": 1610048465270,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "exporter/opentelemetry-exporter-otlp/tests/performance/benchmarks/test_benchmark_trace_exporter.py::test_simple_span_processor",
+            "value": 952.3319906794069,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000034814891805002806",
+            "extra": "mean: 1.050053983051211 msec\nrounds: 177"
+          },
+          {
+            "name": "exporter/opentelemetry-exporter-otlp/tests/performance/benchmarks/test_benchmark_trace_exporter.py::test_batch_span_processor",
+            "value": 3813.032827199533,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0013643331857199018",
+            "extra": "mean: 262.258429265726 usec\nrounds: 5761"
           }
         ]
       }
