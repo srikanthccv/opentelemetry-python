5108f3cf48be6a993f06a492035b31d3b27e0c55
github-action-benchmark <github@users.noreply.github.com>
2021-02-19 14:07:40 +0000

    add OpenTelemetry Python Benchmarks - Python 3.8 - propagator (pytest) benchmark result for 03c6b7391d16236ad5d4e48b0cb0bff786c2e583

--- benchmarks/data.js
+++ benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1613743657322,
+  "lastUpdate": 1613743660818,
   "repoUrl": "https://github.com/lonewolf3739/opentelemetry-python",
   "entries": {
     "OpenTelemetry Python Benchmarks - Python 3.7 - core": [
@@ -6524,6 +6524,44 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 9.961219130145788e-7",
             "extra": "mean: 4.177358831880271 usec\nrounds: 71429"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "rob.knox.dev@gmail.com",
+            "name": "Rob Knox",
+            "username": "robwknox"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "03c6b7391d16236ad5d4e48b0cb0bff786c2e583",
+          "message": "Zipkin exporter add support for v1 api json format (#1411)",
+          "timestamp": "2021-02-17T12:03:29-08:00",
+          "tree_id": "d7a5fe453f77df1d3e7d772741c3e902040de07a",
+          "url": "https://github.com/lonewolf3739/opentelemetry-python/commit/03c6b7391d16236ad5d4e48b0cb0bff786c2e583"
+        },
+        "date": 1613743656389,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "propagator/opentelemetry-propagator-b3/tests/performance/benchmarks/trace/propagation/test_benchmark_b3_format.py::test_extract_single_header",
+            "value": 86236.94426545863,
+            "unit": "iter/sec",
+            "range": "stddev: 6.140530154992782e-7",
+            "extra": "mean: 11.595958188427373 usec\nrounds: 12078"
+          },
+          {
+            "name": "propagator/opentelemetry-propagator-b3/tests/performance/benchmarks/trace/propagation/test_benchmark_b3_format.py::test_inject_empty_context",
+            "value": 177399.83695637682,
+            "unit": "iter/sec",
+            "range": "stddev: 6.276392753475287e-7",
+            "extra": "mean: 5.636983760283292 usec\nrounds: 62501"
           }
         ]
       }
