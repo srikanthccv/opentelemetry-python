2754b0352c40bcb2222c62ee7af6e5a20b2bc320
github-action-benchmark <github@users.noreply.github.com>
2021-02-27 06:54:46 +0000

    add OpenTelemetry Python Benchmarks - Python 3.5 - propagator (pytest) benchmark result for 290478e3312debd023f179fe403ede025fbbefc7

--- benchmarks/data.js
+++ benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1614408882694,
+  "lastUpdate": 1614408886373,
   "repoUrl": "https://github.com/lonewolf3739/opentelemetry-python",
   "entries": {
     "OpenTelemetry Python Benchmarks - Python 3.7 - core": [
@@ -7438,6 +7438,44 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.0000010128707065228551",
             "extra": "mean: 8.75920478262707 usec\nrounds: 49262"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "lechen@microsoft.com",
+            "name": "Leighton Chen",
+            "username": "lzchen"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "290478e3312debd023f179fe403ede025fbbefc7",
+          "message": "Rename IdsGenerator to IdGenerator (#1651)",
+          "timestamp": "2021-02-26T14:56:23-08:00",
+          "tree_id": "ca5968f581d439ec07d73dc2e9affee198585e3b",
+          "url": "https://github.com/lonewolf3739/opentelemetry-python/commit/290478e3312debd023f179fe403ede025fbbefc7"
+        },
+        "date": 1614408884793,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "propagator/opentelemetry-propagator-b3/tests/performance/benchmarks/trace/propagation/test_benchmark_b3_format.py::test_extract_single_header",
+            "value": 38920.985920446554,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00001980767503994102",
+            "extra": "mean: 25.693079873258426 usec\nrounds: 7950"
+          },
+          {
+            "name": "propagator/opentelemetry-propagator-b3/tests/performance/benchmarks/trace/propagation/test_benchmark_b3_format.py::test_inject_empty_context",
+            "value": 100400.72076412705,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00003888139110723879",
+            "extra": "mean: 9.960087859820403 usec\nrounds: 47394"
           }
         ]
       }
