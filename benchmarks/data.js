edb234b34492b019cbbf3c1ce818e8c1f3aede8e
github-action-benchmark <github@users.noreply.github.com>
2021-03-04 03:32:31 +0000

    add OpenTelemetry Python Benchmarks - Python 3.9 - core (pytest) benchmark result for 9bf28fb451a85fd9e9a4f2276c3eebd484e55d02

--- benchmarks/data.js
+++ benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1614828724462,
+  "lastUpdate": 1614828751186,
   "repoUrl": "https://github.com/lonewolf3739/opentelemetry-python",
   "entries": {
     "OpenTelemetry Python Benchmarks - Python 3.7 - core": [
@@ -1294,6 +1294,44 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.00007227052637411126",
             "extra": "mean: 72.34781353846857 usec\nrounds: 5953"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "aboten@lightstep.com",
+            "name": "alrex",
+            "username": "codeboten"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": false,
+          "id": "9bf28fb451a85fd9e9a4f2276c3eebd484e55d02",
+          "message": "rename BaggagePropagator to W3CBaggagePropagator (#1663)\n\n\r\nCo-authored-by: Leighton Chen <lechen@microsoft.com>",
+          "timestamp": "2021-03-02T17:05:32-08:00",
+          "tree_id": "3c56d146c21a8f8814fba92f760ae3ecbb0e0915",
+          "url": "https://github.com/lonewolf3739/opentelemetry-python/commit/9bf28fb451a85fd9e9a4f2276c3eebd484e55d02"
+        },
+        "date": 1614828749904,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "opentelemetry-sdk/tests/performance/benchmarks/trace/test_benchmark_trace.py::test_simple_start_span",
+            "value": 20621.360718855743,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000015423217592993358",
+            "extra": "mean: 48.493405145937864 usec\nrounds: 4586"
+          },
+          {
+            "name": "opentelemetry-sdk/tests/performance/benchmarks/trace/test_benchmark_trace.py::test_simple_start_as_current_span",
+            "value": 14955.68396424698,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000010953798833574493",
+            "extra": "mean: 66.86421044939152 usec\nrounds: 8211"
           }
         ]
       }
