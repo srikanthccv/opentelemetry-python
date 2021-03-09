d772b0bb61e692e49601bd3aaa7f0865c117a4d3
github-action-benchmark <github@users.noreply.github.com>
2021-03-09 05:26:23 +0000

    add OpenTelemetry Python Benchmarks - Python 3.8 - core (pytest) benchmark result for 6087aea507e5acd974e965e2b16884df02612e57

--- benchmarks/data.js
+++ benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1615267580992,
+  "lastUpdate": 1615267583050,
   "repoUrl": "https://github.com/lonewolf3739/opentelemetry-python",
   "entries": {
     "OpenTelemetry Python Benchmarks - Python 3.7 - core": [
@@ -2856,6 +2856,44 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.0000013116443138239284",
             "extra": "mean: 45.44413746009442 usec\nrounds: 6882"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "aboten@lightstep.com",
+            "name": "alrex",
+            "username": "codeboten"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "6087aea507e5acd974e965e2b16884df02612e57",
+          "message": "expose StatusCode from opentelemetry.trace (#1681)",
+          "timestamp": "2021-03-08T14:15:58-08:00",
+          "tree_id": "8cdba8ede37736f73416e5d39b034c9586cfca34",
+          "url": "https://github.com/lonewolf3739/opentelemetry-python/commit/6087aea507e5acd974e965e2b16884df02612e57"
+        },
+        "date": 1615267577003,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "opentelemetry-sdk/tests/performance/benchmarks/trace/test_benchmark_trace.py::test_simple_start_span",
+            "value": 26667.296914832536,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0000012521447990403967",
+            "extra": "mean: 37.499113734463016 usec\nrounds: 4871"
+          },
+          {
+            "name": "opentelemetry-sdk/tests/performance/benchmarks/trace/test_benchmark_trace.py::test_simple_start_as_current_span",
+            "value": 19136.51289374107,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0000016867301301279692",
+            "extra": "mean: 52.256124485828735 usec\nrounds: 7294"
           }
         ]
       }
