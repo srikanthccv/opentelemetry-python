1a4182bcd332c45f6e2946810330efa101e93e18
github-action-benchmark <github@users.noreply.github.com>
2021-06-26 00:39:57 +0000

    add OpenTelemetry Python Benchmarks - Python 3.7 - exporter (pytest) benchmark result for 01c6954c556149fa7881414c5f50050b9322a138

--- benchmarks/data.js
+++ benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1624667990116,
+  "lastUpdate": 1624667997306,
   "repoUrl": "https://github.com/lonewolf3739/opentelemetry-python",
   "entries": {
     "OpenTelemetry Python Benchmarks - Python 3.7 - core": [
@@ -10996,6 +10996,44 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.00092269507676688",
             "extra": "mean: 341.4538000003552 usec\nrounds: 4155"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "aboten@lightstep.com",
+            "name": "alrex",
+            "username": "codeboten"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "01c6954c556149fa7881414c5f50050b9322a138",
+          "message": "use BoundedAttributes for attributes in link, event, resource, spans (#1915)",
+          "timestamp": "2021-06-25T09:21:11-07:00",
+          "tree_id": "d0458d87ddf43afe81966a7d515e891fde21bc4b",
+          "url": "https://github.com/lonewolf3739/opentelemetry-python/commit/01c6954c556149fa7881414c5f50050b9322a138"
+        },
+        "date": 1624667995718,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "exporter/opentelemetry-exporter-otlp-proto-grpc/tests/performance/benchmarks/test_benchmark_trace_exporter.py::test_simple_span_processor",
+            "value": 2175.580017989766,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000014684578177044243",
+            "extra": "mean: 459.6475384637881 usec\nrounds: 39"
+          },
+          {
+            "name": "exporter/opentelemetry-exporter-otlp-proto-grpc/tests/performance/benchmarks/test_benchmark_trace_exporter.py::test_batch_span_processor",
+            "value": 2930.7952191356153,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0009556646118261685",
+            "extra": "mean: 341.2043234787764 usec\nrounds: 4059"
           }
         ]
       }
