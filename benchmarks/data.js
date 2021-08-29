7cad770323efa97bf4f6c591f1cf8409066fa25e
github-action-benchmark <github@users.noreply.github.com>
2021-08-29 10:32:31 +0000

    add OpenTelemetry Python Benchmarks - Python 3.9 - core (pytest) benchmark result for c3dfa360e643d8e8039e430c468522b5ab93b1c7

--- benchmarks/data.js
+++ benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1630233138383,
+  "lastUpdate": 1630233151005,
   "repoUrl": "https://github.com/lonewolf3739/opentelemetry-python",
   "entries": {
     "OpenTelemetry Python Benchmarks - Python 3.7 - core": [
@@ -3916,6 +3916,44 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.00006729045869687694",
             "extra": "mean: 69.86433695134164 usec\nrounds: 6725"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "owais@users.noreply.github.com",
+            "name": "Owais Lone",
+            "username": "owais"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": false,
+          "id": "c3dfa360e643d8e8039e430c468522b5ab93b1c7",
+          "message": "Use exact dep versions in getting started docs tests (#2070)",
+          "timestamp": "2021-08-27T10:43:15-07:00",
+          "tree_id": "8db6ad28222f87261a398e99dedc85a67a2a1d50",
+          "url": "https://github.com/lonewolf3739/opentelemetry-python/commit/c3dfa360e643d8e8039e430c468522b5ab93b1c7"
+        },
+        "date": 1630233148886,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "opentelemetry-sdk/tests/performance/benchmarks/trace/test_benchmark_trace.py::test_simple_start_span",
+            "value": 25080.16782566879,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000014118887838300277",
+            "extra": "mean: 39.872141484497185 usec\nrounds: 5308"
+          },
+          {
+            "name": "opentelemetry-sdk/tests/performance/benchmarks/trace/test_benchmark_trace.py::test_simple_start_as_current_span",
+            "value": 16828.02932206032,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00004396815914666512",
+            "extra": "mean: 59.42466469850233 usec\nrounds: 7113"
           }
         ]
       }
