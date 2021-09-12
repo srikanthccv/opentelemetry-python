35e73c3070c88fef000ed109d327a70cd0dcd56f
github-action-benchmark <github@users.noreply.github.com>
2021-09-12 17:15:25 +0000

    add OpenTelemetry Python Benchmarks - Python pypy-3.7 - core (pytest) benchmark result for df1f84243178127f8fbced191f1ed7cb99c214f6

--- benchmarks/data.js
+++ benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1631466910742,
+  "lastUpdate": 1631466925210,
   "repoUrl": "https://github.com/lonewolf3739/opentelemetry-python",
   "entries": {
     "OpenTelemetry Python Benchmarks - Python 3.7 - core": [
@@ -29182,6 +29182,44 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.00004037609716810434",
             "extra": "mean: 5.190729812749641 usec\nrounds: 117661"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "srikanth.chekuri92@gmail.com",
+            "name": "Srikanth Chekuri",
+            "username": "lonewolf3739"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": false,
+          "id": "df1f84243178127f8fbced191f1ed7cb99c214f6",
+          "message": "Do not skip sequence attribute on decode error (#2097)\n\nDo not skip sequence attribute on decode error\r\n\r\nCo-authored-by: Owais Lone <owais@users.noreply.github.com>",
+          "timestamp": "2021-09-10T03:40:49Z",
+          "tree_id": "7b28187c0875d569a7706f5a34283911e55258e5",
+          "url": "https://github.com/lonewolf3739/opentelemetry-python/commit/df1f84243178127f8fbced191f1ed7cb99c214f6"
+        },
+        "date": 1631466922217,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "opentelemetry-sdk/tests/performance/benchmarks/trace/test_benchmark_trace.py::test_simple_start_span",
+            "value": 255209.0703334594,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000011288238451208863",
+            "extra": "mean: 3.9183560313643526 usec\nrounds: 163935"
+          },
+          {
+            "name": "opentelemetry-sdk/tests/performance/benchmarks/trace/test_benchmark_trace.py::test_simple_start_as_current_span",
+            "value": 160176.9876501885,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00004759079789422583",
+            "extra": "mean: 6.243094059078612 usec\nrounds: 101011"
           }
         ]
       }
